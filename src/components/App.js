0d54bc75c1ebf2b9b502c37a24e60a7528dbaf22
Faiza Aftab <102207534+faiza-ahmad@users.noreply.github.com>
2022-11-04 20:43:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect } from "react";
 import '../styles/App.css';
+import './App.css'
 
 class App extends Component {
     constructor(props) {
@@ -7,17 +8,18 @@ class App extends Component {
         this.state = {
             renderBall: false,
             posi : 0,
-            ballPosition: { left: "0px" }
+            ballPosition: { left: 0 }
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
     };
 
     buttonClickHandler() {
-   
+        this.setState({renderBall: true})
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
+            console.log(this.state.ballPosition.left);
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
 		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
@@ -26,10 +28,24 @@ class App extends Component {
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+
+            document.addEventListener('keydown', (e)=>{
+                if(e.key == 'ArrowRight'){
+                    console.log(e.key);
+                    
+                    // this.setState((preState)=>({
+                    //     posi : preState.posi +5
+                    // }))
+                    console.log(this.state.posi);
+                    this.setState({ballPosition: {left: (this.state.ballPosition.left + 5)}})
+                }
+          
+            });
     }
+   
 
     render() {
+        
         return (
             <div className="playground">
                 {this.renderBallOrButton()}
